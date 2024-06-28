a024c415914ad06d02f3550a437cc46260ccbf3e
Imad Asserne <imad.asserne@gmail.com>
2024-06-28 15:04:45 +0100

    change dependency

--- src/main/java/com/hrafty/web_app/controller/SellerController.java
+++ src/main/java/com/hrafty/web_app/controller/SellerController.java
@@ -1,7 +1,7 @@
 package com.hrafty.web_app.controller;
 
 import com.hrafty.web_app.dto.ServiceDTO;
-import com.hrafty.web_app.services.ServicesService;
+import com.hrafty.web_app.services.Service;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,33 +11,33 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/v1/seller")
 public class SellerController {
-    private  final ServicesService serviceService;
+    private  final Service service;
 
-    public SellerController(ServicesService serviceService) {
-        this.serviceService = serviceService;
+    public SellerController(Service service) {
+        this.service = service;
     }
 
 
     @PostMapping(path = "/add")
     public ResponseEntity<ServiceDTO> createService(@RequestBody ServiceDTO serviceDTO){
-        ServiceDTO serviceDTO1= serviceService.create(serviceDTO);
+        ServiceDTO serviceDTO1= service.create(serviceDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(serviceDTO1);
     }
 
     @GetMapping(path ="/getAll/{id}")
     public ResponseEntity<List<ServiceDTO>> getAllServices(@PathVariable("id") Long id) {
-        List<ServiceDTO> services = serviceService.getAllServices(id);
+        List<ServiceDTO> services = service.getAllServices(id);
         return new ResponseEntity<>(services, HttpStatus.OK);
     }
 
     @PutMapping(path = "/update/{id}")
     public ResponseEntity<ServiceDTO> updateService(@PathVariable("id") Long id,@RequestBody ServiceDTO serviceDTO){
-        serviceService.updateService(id,serviceDTO);
+        service.updateService(id,serviceDTO);
         return  ResponseEntity.status(HttpStatus.OK).body(serviceDTO);
     }
     @DeleteMapping(path = "/delete/{id}")
     public ResponseEntity<String> deleteService(@PathVariable("id") Long id){
-        serviceService.deleteService(id);
+        service.deleteService(id);
         return new ResponseEntity<>("Serive successfully deleted (: ",HttpStatus.OK);
     }
 }
