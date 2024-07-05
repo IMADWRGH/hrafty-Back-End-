9f23f1d914bbd1477def7e57a83b4f378311388a
Imad Asserne <imad.asserne@gmail.com>
2024-07-05 11:42:06 +0100

    add methods

--- src/main/java/com/hrafty/web_app/services/Order.java
+++ src/main/java/com/hrafty/web_app/services/Order.java
@@ -1,4 +1,15 @@
 package com.hrafty.web_app.services;
 
+import com.hrafty.web_app.dto.OrderDTO;
+import com.hrafty.web_app.dto.OrderItemDTO;
+
+import java.util.List;
+
 public interface Order {
+    OrderDTO create(Long customerId, List<OrderItemDTO> orderItemsDTO) throws Exception;
+    List<OrderDTO> getAllOrders();
+    List<OrderDTO> getAllOrders(Long id);
+    void updateOrders(Long id , OrderDTO orderDTO);
+    OrderDTO getOrder(Long id);
+    void  deleteOrder(Long id);
 }
