d9ec085186d7fff03d5c9bb034165eb83b0732ec
Imad Asserne <imad.asserne@gmail.com>
2024-07-05 11:51:35 +0100

    fix error

--- src/main/java/com/hrafty/web_app/mapper/AddressMapper.java
+++ src/main/java/com/hrafty/web_app/mapper/AddressMapper.java
@@ -3,6 +3,7 @@ package com.hrafty.web_app.mapper;
 import com.hrafty.web_app.dto.AddressDTO;
 import com.hrafty.web_app.entities.Address;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring",uses = {SellerMapper.class,CustomerMapper.class})
@@ -25,6 +26,13 @@ public interface AddressMapper {
         }
         return address.getId();
     }
+    @Mapping(source = "shop_number", target = "shop_number")
+    @Mapping(source = "name_city", target = "name_city")
+    @Mapping(source = "name_regional", target = "name_regional")
     AddressDTO toDTO(Address entity);
+
+    @Mapping(source = "shop_number", target = "shop_number")
+    @Mapping(source = "name_city", target = "name_city")
+    @Mapping(source = "name_regional", target = "name_regional")
     Address toEntity(AddressDTO dto);
 }
