59177fcd725da0605900d70151177ddfe9970e36
Imad Asserne <imad.asserne@gmail.com>
2024-07-05 19:57:54 +0100

    add methods

--- src/main/java/com/hrafty/web_app/Repository/ServiceRepository.java
+++ src/main/java/com/hrafty/web_app/Repository/ServiceRepository.java
@@ -2,9 +2,12 @@ package com.hrafty.web_app.Repository;
 
 import com.hrafty.web_app.entities.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface ServiceRepository extends JpaRepository<Service,Long> {
@@ -14,5 +17,6 @@ public interface ServiceRepository extends JpaRepository<Service,Long> {
 
     List<Service> findAllByType(String type);
 
-//    List<String> findDistinctType();
+    @Query("SELECT S.type FROM Service S")
+    Set<String> findAllType();
 }
