517454d81d21cf62e359241328d16f660bfab901
Imad Asserne <imad.asserne@gmail.com>
2024-07-08 14:59:28 +0100

    change to record

--- src/main/java/com/hrafty/web_app/dto/PanelDTO.java
+++ src/main/java/com/hrafty/web_app/dto/PanelDTO.java
@@ -2,50 +2,5 @@ package com.hrafty.web_app.dto;
 
 import java.util.List;
 
-public class PanelDTO {
-    private Long id;
-    private CustomerDTO customer;
-    private List<Long> services;
-
-    public PanelDTO() {
-    }
-
-    public PanelDTO(Long id, CustomerDTO customer, List<Long> services) {
-        this.id = id;
-        this.customer = customer;
-        this.services = services;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public CustomerDTO getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(CustomerDTO customer) {
-        this.customer = customer;
-    }
-
-    public List<Long> getServices() {
-        return services;
-    }
-
-    public void setServices(List<Long> services) {
-        this.services = services;
-    }
-
-    @Override
-    public String toString() {
-        return "PanelDTO{" +
-                "id=" + id +
-                ", customer=" + customer +
-                ", services=" + services +
-                '}';
-    }
+public record PanelDTO(Long id,CustomerDTO customer,List<Long> services) {
 }
--- src/main/java/com/hrafty/web_app/dto/UserDTO.java
+++ src/main/java/com/hrafty/web_app/dto/UserDTO.java
@@ -1,73 +1,8 @@
 package com.hrafty.web_app.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.hrafty.web_app.entities.Role;
 
 
-public class UserDTO {
-    private Long id;
-    private String fullName;
-    private String email;
-    private String password;
-    private Role role;
-
-
-    public UserDTO(Long id, String fullName, String email, String password, Role role) {
-        this.id = id;
-        this.fullName = fullName;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    @Override
-    public String toString() {
-        return "UserDTO{" +
-                "id=" + id +
-                ", full_name='" + fullName + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", role=" + role +
-                '}';
-    }
+public record UserDTO(Long id,String fullName,String email,String password,Role role) {
 }
 
