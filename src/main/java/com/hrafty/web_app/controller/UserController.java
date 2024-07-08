ab96012e2a474a2b87f116ad58468ec03221f144
Imad Asserne <imad.asserne@gmail.com>
2024-07-08 15:03:07 +0100

    add new endpoint

--- src/main/java/com/hrafty/web_app/controller/UserController.java
+++ src/main/java/com/hrafty/web_app/controller/UserController.java
@@ -1,14 +1,37 @@
 package com.hrafty.web_app.controller;
 
+import com.hrafty.web_app.dto.ProductDTO;
+import com.hrafty.web_app.services.ProductService;
+import com.hrafty.web_app.services.ServiceService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(path = "api/v1/admin")
+@RequestMapping(path = "api/v1/user")
 public class UserController {
+    private final ProductService product;
+    private  final ServiceService service;
+    public UserController(ProductService product, ServiceService service) {
+        this.product = product;
+        this.service = service;
+    }
 
+    @GetMapping(path ="/getAll")
+    public ResponseEntity<List<ProductDTO>> getAllProducts() {
+        List<ProductDTO> products = product.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
 
+    @GetMapping(path = "/ServicesCategories")
+    public ResponseEntity<List<String>> getAllType(){
+        return ResponseEntity.status(HttpStatus.OK).body(service.getAllTypes());
+    }
 
-
-
-
+    @GetMapping(path = "/ProductsCategories")
+    public ResponseEntity<List<String>> getAllTypes(){
+        return ResponseEntity.status(HttpStatus.OK).body(product.getAllCategories());
+    }
 }
