1b3663d9d525505414464d40fc713cf3c6f53acb
imad ASSERNE <imad.asserne@gmail.com>
2026-02-09 23:48:16 +0100

    feat: add more methods to AddressService interface

--- src/main/java/com/hrafty/web_app/services/AddressService.java
+++ src/main/java/com/hrafty/web_app/services/AddressService.java
@@ -2,12 +2,16 @@ package com.hrafty.web_app.services;
 
 import com.hrafty.web_app.dto.AddressDTO;
 
+import java.util.List;
 import java.util.Set;
 
 public interface AddressService {
 
+   List<AddressDTO> getAllAddresses();
+   AddressDTO getAddressById(Long id);
    AddressDTO getAddressSeller(Long id);
+   AddressDTO createAddress(AddressDTO addressDTO);
+   AddressDTO updateAddress(Long id, AddressDTO updateAddressDTO);
+   void deleteAddress(Long id);
    Set<String> getAllCities();
-
-   AddressDTO updateAddress(Long id,AddressDTO updateAddressDTO);
 }
