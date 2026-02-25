1296f137bbac9fe8f1b19bcda8feb405a93dd381
imad ASSERNE <imad.asserne@gmail.com>
2026-02-25 23:22:21 +0100

    feat: add PaymentStatus enum

---
+++ src/main/java/com/hrafty/web_app/entities/enums/PaymentStatus.java
@@ -0,0 +1,12 @@
+package com.hrafty.web_app.entities.enums;
+
+public enum PaymentStatus {
+    PENDING,
+    PROCESSING,
+    COMPLETED,
+    FAILED,
+    REFUNDED,
+    PARTIALLY_REFUNDED,
+    CANCELLED,
+    DECLINED
+}
