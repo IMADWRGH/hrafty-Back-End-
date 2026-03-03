21365959b81b17d984cad287c70791efe319e90d
imad ASSERNE <imad.asserne@gmail.com>
2026-03-03 03:54:44 +0100

    feat: add setupToken field to EmailVerifiedResponseDTO

--- src/main/java/com/hrafty/web_app/dto/response/EmailVerifiedResponseDTO.java
+++ src/main/java/com/hrafty/web_app/dto/response/EmailVerifiedResponseDTO.java
@@ -3,5 +3,6 @@ package com.hrafty.web_app.dto.response;
 public record EmailVerifiedResponseDTO(
         String message,
         Long userId,
-        String email
+        String email,
+        String setupToken
 ) {}
