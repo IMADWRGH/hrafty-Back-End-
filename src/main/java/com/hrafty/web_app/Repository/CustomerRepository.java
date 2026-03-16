9d176024273b386432c31c40ce9b6c818bc389ce
imad ASSERNE <imad.asserne@gmail.com>
2026-03-16 23:54:58 +0100

    feat: add existsByUserId query method

--- src/main/java/com/hrafty/web_app/Repository/CustomerRepository.java
+++ src/main/java/com/hrafty/web_app/Repository/CustomerRepository.java
@@ -5,4 +5,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<com.hrafty.web_app.entities.Customer,Long> {
+
+    boolean existsByUserId(Long userId);
+
 }
