c5c3d9a5ec777e19a6b1c24a8f10909ab3c14be0
imad ASSERNE <imad.asserne@gmail.com>
2026-03-16 23:52:36 +0100

    feat: add new methods to interface

--- src/main/java/com/hrafty/web_app/services/AddressService.java
+++ src/main/java/com/hrafty/web_app/services/AddressService.java
@@ -1,6 +1,7 @@
 package com.hrafty.web_app.services;
 
-import com.hrafty.web_app.dto.AddressDTO;
+import com.hrafty.web_app.dto.common.AddressDTO;
+import com.hrafty.web_app.dto.response.AddressResponseDTO;
 
 import java.util.List;
 import java.util.Set;
@@ -14,4 +15,6 @@ public interface AddressService {
    AddressDTO updateAddress(Long id, AddressDTO updateAddressDTO);
    void deleteAddress(Long id);
    Set<String> getAllCities();
+   AddressResponseDTO getAddressByCustomerId(Long customerId);
+   boolean existsById(Long id);
 }
