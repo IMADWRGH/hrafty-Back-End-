e9b5013e007934f10e015c9f8bf9a0e43c4617ac
imad ASSERNE <imad.asserne@gmail.com>
2026-03-17 00:00:28 +0100

    refactor: enhance interface with complete image management methods

--- src/main/java/com/hrafty/web_app/services/ImageService.java
+++ src/main/java/com/hrafty/web_app/services/ImageService.java
@@ -1,24 +1,46 @@
 package com.hrafty.web_app.services;
 
 
-import com.hrafty.web_app.dto.ImageDTO;
+import com.hrafty.web_app.dto.common.ImageDTO;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
-import java.util.Set;
 
 public interface ImageService {
     String uploadFile(MultipartFile file);
-     void deleteFile(List<String> blobFilesUrl);
+
+    void deleteFile(List<String> blobFilesUrl);
+
     List<ImageDTO> uploadFiles(List<MultipartFile> files);
 
-   List<String> updateFile(List<String> blobFilesUrl, List<MultipartFile> files);
+    List<String> updateFile(List<String> blobFilesUrl, List<MultipartFile> files);
+
     ImageDTO create(ImageDTO dto);
+
     ImageDTO getById(Long id);
+
     List<ImageDTO> getAll();
+
     String getImageUrlWithToken(String imageUrl);
-    List<ImageDTO> getAllImageByProduct(Long id);
-    List<ImageDTO> getAllImageByService(Long id);
-    List<ImageDTO> getAllImageByProducts();
-    List<ImageDTO> getAllImageByServices();
+
+    List<ImageDTO> getAllImagesByProduct(Long productId);
+
+    List<ImageDTO> getAllImagesByService(Long serviceId);
+
+    List<ImageDTO> getAllImagesByProducts();
+
+    List<ImageDTO> getAllImagesByServices();
+
+
+    void deleteImage(Long id);
+
+    void deleteImagesByProduct(Long productId);
+
+    void deleteImagesByService(Long serviceId);
+
+    ImageDTO setPrimaryImage(Long imageId);
+
+    List<ImageDTO> reorderImages(List<Long> imageIds);
+
+    boolean existsById(Long id);
 }
