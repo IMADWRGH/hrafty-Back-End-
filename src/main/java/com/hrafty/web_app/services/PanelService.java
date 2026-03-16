34b983f3cbd6303248822e6b1d229d217b4833c5
imad ASSERNE <imad.asserne@gmail.com>
2026-03-16 23:49:22 +0100

    feat: add new methods to PanelService interface

--- src/main/java/com/hrafty/web_app/services/PanelService.java
+++ src/main/java/com/hrafty/web_app/services/PanelService.java
@@ -1,15 +1,38 @@
 package com.hrafty.web_app.services;
 
-import com.hrafty.web_app.dto.PanelDTO;
+import com.hrafty.web_app.dto.request.PanelRequestDTO;
+import com.hrafty.web_app.dto.response.PanelResponseDTO;
+import com.hrafty.web_app.dto.common.PageResponseDTO;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
-
 public interface PanelService {
-    public PanelDTO getPanelById(Long id);
-    public PanelDTO create(PanelDTO panelDTO);
-    public PanelDTO createById(PanelDTO panelDTO , Long customerId);
-    public PanelDTO updatePanel(Long id, PanelDTO updatedPanelDTO);
-    public void deletePanel(Long id);
-    public List<PanelDTO> getPanelsByCustomer(Long customerId);
+
+    PanelResponseDTO getPanelById(Long id);
+
+    PanelResponseDTO getPanelByCustomerId(Long customerId);
+
+    PanelResponseDTO create(PanelRequestDTO panelRequestDTO);
+
+    PanelResponseDTO createByCustomerId(PanelRequestDTO panelRequestDTO, Long customerId);
+
+    PanelResponseDTO updatePanel(Long id, PanelRequestDTO updatedPanelRequestDTO);
+
+    void deletePanel(Long id);
+
+    List<PanelResponseDTO> getAllPanels();
+
+    PageResponseDTO<PanelResponseDTO> getAllPanels(Pageable pageable);
+
+    List<PanelResponseDTO> getPanelsByCustomerId(Long customerId);
+
+    // NEW METHODS
+    PanelResponseDTO addServiceToPanel(Long panelId, Long serviceId);
+
+    PanelResponseDTO removeServiceFromPanel(Long panelId, Long serviceId);
+
+    boolean existsById(Long id);
+
+    boolean existsByCustomerId(Long customerId);
 }
