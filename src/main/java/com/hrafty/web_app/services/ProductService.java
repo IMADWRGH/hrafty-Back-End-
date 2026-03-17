98186dcaf317ced601dc054b1cc5e45a4298b70d
imad ASSERNE <imad.asserne@gmail.com>
2026-03-17 02:10:45 +0100

    feat: expand interface with complete product operations

--- src/main/java/com/hrafty/web_app/services/ProductService.java
+++ src/main/java/com/hrafty/web_app/services/ProductService.java
@@ -1,19 +1,57 @@
 package com.hrafty.web_app.services;
 
-import com.hrafty.web_app.dto.ProductDTO;
+import com.hrafty.web_app.dto.request.ProductRequestDTO;
+import com.hrafty.web_app.dto.response.ProductResponseDTO;
+import com.hrafty.web_app.dto.response.ProductSummaryDTO;
+import com.hrafty.web_app.dto.common.PageResponseDTO;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Set;
 
 public interface ProductService {
-    ProductDTO create(ProductDTO productDTO,List<MultipartFile> files);
-    List<ProductDTO> getAllProducts();
-    List<ProductDTO> getAllProducts(Long id);
-    Set<String> getAllCategories();
-    void updateProduct(Long id , ProductDTO productDT,List<MultipartFile> files);
-    ProductDTO getProduct(Long id);
-    void  deleteProduct(Long id);
 
-    public List<ProductDTO> getAllProducts(String category);
+    ProductResponseDTO create(ProductRequestDTO productRequestDTO, List<MultipartFile> files);
+
+    List<ProductResponseDTO> getAllProducts();
+
+    PageResponseDTO<ProductResponseDTO> getAllProducts(Pageable pageable);
+
+    List<ProductResponseDTO> getProductsBySellerId(Long sellerId);
+
+    PageResponseDTO<ProductResponseDTO> getProductsBySellerId(Long sellerId, Pageable pageable);
+
+    Set<String> getAllCategories();
+
+    ProductResponseDTO updateProduct(Long id, ProductRequestDTO productRequestDTO, List<MultipartFile> files);
+
+    ProductResponseDTO getProduct(Long id);
+
+    void deleteProduct(Long id);
+
+    List<ProductResponseDTO> getProductsByCategory(String category);
+
+    PageResponseDTO<ProductResponseDTO> getProductsByCategory(String category, Pageable pageable);
+
+
+    List<ProductSummaryDTO> getProductSummaries();
+
+    PageResponseDTO<ProductSummaryDTO> getProductSummaries(Pageable pageable);
+
+    List<ProductResponseDTO> searchProducts(String keyword);
+
+    List<ProductResponseDTO> getActiveProducts();
+
+    List<ProductResponseDTO> getProductsByPriceRange(double minPrice, double maxPrice);
+
+    ProductResponseDTO updateStock(Long id, Integer newStock);
+
+    ProductResponseDTO toggleActiveStatus(Long id);
+
+    boolean existsById(Long id);
+
+    long countProducts();
+
+    long countProductsBySellerId(Long sellerId);
 }
