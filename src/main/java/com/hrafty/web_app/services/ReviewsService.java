5069ca45809fbdb773ff9bf65d8e2fb4afbddd60
imad ASSERNE <imad.asserne@gmail.com>
2026-03-17 02:11:09 +0100

    feat: expand interface with complete Reviews operations

--- src/main/java/com/hrafty/web_app/services/ReviewsService.java
+++ src/main/java/com/hrafty/web_app/services/ReviewsService.java
@@ -1,16 +1,48 @@
 package com.hrafty.web_app.services;
 
-import com.hrafty.web_app.dto.ReviewsDTO;
+import com.hrafty.web_app.dto.request.ReviewRequestDTO;
+import com.hrafty.web_app.dto.response.ReviewResponseDTO;
+import com.hrafty.web_app.dto.common.PageResponseDTO;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface ReviewsService {
-    ReviewsDTO create(ReviewsDTO reviewsDTO);
-    List<ReviewsDTO> getAllReviews();
-    List<ReviewsDTO> getAllReviews(Long id);
-    void updateReviews(Long id , ReviewsDTO productDTO);
-    ReviewsDTO getReview(Long id);
-    void  deleteReview(Long id);
 
-    ReviewsDTO updateReview(Long id,ReviewsDTO updatedReviewsDTO);
+    ReviewResponseDTO create(ReviewRequestDTO reviewRequestDTO);
+
+    List<ReviewResponseDTO> getAllReviews();
+
+    PageResponseDTO<ReviewResponseDTO> getAllReviews(Pageable pageable);
+
+    List<ReviewResponseDTO> getReviewsByProductId(Long productId);
+
+    PageResponseDTO<ReviewResponseDTO> getReviewsByProductId(Long productId, Pageable pageable);
+
+    List<ReviewResponseDTO> getReviewsByServiceId(Long serviceId);
+
+    PageResponseDTO<ReviewResponseDTO> getReviewsByServiceId(Long serviceId, Pageable pageable);
+
+    List<ReviewResponseDTO> getReviewsByCustomerId(Long customerId);
+
+    ReviewResponseDTO getReview(Long id);
+
+    ReviewResponseDTO updateReview(Long id, ReviewRequestDTO reviewRequestDTO);
+
+    void deleteReview(Long id);
+
+
+    Double getAverageRatingByProductId(Long productId);
+
+    Double getAverageRatingByServiceId(Long serviceId);
+
+    long countReviewsByProductId(Long productId);
+
+    long countReviewsByServiceId(Long serviceId);
+
+    boolean existsById(Long id);
+
+    boolean hasCustomerReviewedProduct(Long customerId, Long productId);
+
+    boolean hasCustomerReviewedService(Long customerId, Long serviceId);
 }
