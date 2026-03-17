16dfa2aeece54bc5104fbe28546b5be8205c3006
imad ASSERNE <imad.asserne@gmail.com>
2026-03-17 02:09:04 +0100

    feat: expand interface with complete order operations

--- src/main/java/com/hrafty/web_app/services/OrderService.java
+++ src/main/java/com/hrafty/web_app/services/OrderService.java
@@ -1,15 +1,44 @@
 package com.hrafty.web_app.services;
 
-import com.hrafty.web_app.dto.OrderDTO;
-import com.hrafty.web_app.dto.OrderItemDTO;
+
+
+import com.hrafty.web_app.dto.common.PageResponseDTO;
+import com.hrafty.web_app.dto.request.OrderRequestDTO;
+import com.hrafty.web_app.dto.response.OrderResponseDTO;
+import com.hrafty.web_app.entities.enums.OrderStatus;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface OrderService {
-    OrderDTO create(Long customerId, List<OrderItemDTO> orderItemsDTO) throws Exception;
-    List<OrderDTO> getAllOrders();
-    List<OrderDTO> getAllOrders(Long id);
-    void updateOrders(Long id , OrderDTO orderDTO);
-    OrderDTO getOrder(Long id);
-    void  deleteOrder(Long id);
+    OrderResponseDTO create(Long customerId, OrderRequestDTO orderRequestDTO) throws Exception;
+
+    List<OrderResponseDTO> getAllOrders();
+
+    PageResponseDTO<OrderResponseDTO> getAllOrders(Pageable pageable);
+
+    List<OrderResponseDTO> getOrdersByCustomerId(Long customerId);
+
+    PageResponseDTO<OrderResponseDTO> getOrdersByCustomerId(Long customerId, Pageable pageable);
+
+    OrderResponseDTO getOrder(Long id);
+
+    OrderResponseDTO updateOrder(Long id, OrderRequestDTO orderRequestDTO);
+
+    OrderResponseDTO updateOrderStatus(Long id, OrderStatus status);
+
+    void deleteOrder(Long id);
+
+
+    List<OrderResponseDTO> getOrdersByStatus(OrderStatus status);
+
+    PageResponseDTO<OrderResponseDTO> getOrdersByStatus(OrderStatus status, Pageable pageable);
+
+    double calculateTotalRevenue();
+
+    long countOrders();
+
+    long countOrdersByStatus(OrderStatus status);
+
+    boolean existsById(Long id);
 }
