598e88dad0e2d51ab297a6de811e07f40a1ff9c7
imad ASSERNE <imad.asserne@gmail.com>
2026-03-21 22:07:12 +0100

    feat: add @Repository annotation to Order and Image repositories

--- src/main/java/com/hrafty/web_app/Repository/ImageRepository.java
+++ src/main/java/com/hrafty/web_app/Repository/ImageRepository.java
@@ -2,8 +2,10 @@ package com.hrafty.web_app.Repository;
 
 import com.hrafty.web_app.entities.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+@Repository
 public interface ImageRepository extends JpaRepository<Image,Long> {
     List<Image> findByProductId(Long id);
     List<Image> findByServiceId(Long id);
--- src/main/java/com/hrafty/web_app/Repository/OrderRepository.java
+++ src/main/java/com/hrafty/web_app/Repository/OrderRepository.java
@@ -3,11 +3,13 @@ package com.hrafty.web_app.Repository;
 import com.hrafty.web_app.entities.Customer;
 import com.hrafty.web_app.entities.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Set;
 
+@Repository
 public interface OrderRepository extends JpaRepository<Order ,Long> {
     List<Order> findByCustomer(Customer customer);
-    Set<String> findById();
+   // Set<String> findById();
 }
