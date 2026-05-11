0b0210f6cf72b06d48ff0ee1b20ed0977aa46410
imad ASSERNE <imad.asserne@gmail.com>
2026-05-12 00:50:58 +0200

    feat: add new method "findCustomerWithRelations"

--- src/main/java/com/hrafty/web_app/Repository/CustomerRepository.java
+++ src/main/java/com/hrafty/web_app/Repository/CustomerRepository.java
@@ -1,11 +1,25 @@
 package com.hrafty.web_app.Repository;
 
+import com.hrafty.web_app.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<com.hrafty.web_app.entities.Customer,Long> {
 
     boolean existsByUserId(Long userId);
+
+    @Query("""
+    SELECT c
+    FROM Customer c
+    LEFT JOIN FETCH c.user
+    LEFT JOIN FETCH c.panel
+    WHERE c.id = :id
+""")
+    Optional<Customer> findCustomerWithRelations(@Param("id") Long id);
 
 }
